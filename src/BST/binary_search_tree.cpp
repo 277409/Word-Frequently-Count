b00780b88691d960fe644a804da7a44f1d8257fd
Hadeer Ramadan <hadderramadan7@gmail.com>
2024-04-21 13:27:04 +0200

    implementation of Binary Search Tree method : need implementation (deleteNode,editNode,FillTree)

---
+++ src/BST/binary_search_tree.cpp
@@ -0,0 +1,73 @@
+#include "binary_search_tree.hpp"
+
+
+
+//__________________methods__________________//
+
+//insertion
+void BinarySTree::insert(string word, int counter){
+    if(!this->root){// if my tree doesn't have any node, so create first node
+        root=new TreeNode(counter,word);
+        return;
+    }
+    insert(word,counter,this->root);
+}
+void BinarySTree::insert(string word, int counter, TreeNode*root){
+    if(!root)
+    return;
+
+    if(counter>=root->counter){// add in right of the node
+        if(!root->right)
+        root->right=new TreeNode(counter,word);
+        else
+        insert(word,counter,root->right);
+    }
+
+    //add in the left of node (counter of new word < current counter)
+    else{
+        if(!root->left)
+        root->left=new TreeNode(counter,word);
+        else
+        insert(word,counter,root->left);
+    }
+    
+}
+
+
+//deletion
+void BinarySTree::deleteNode(string word,TreeNode*root){
+
+}
+void BinarySTree::deleteNode(string word){
+
+}
+
+//editing word
+void BinarySTree::editNode(string word){
+
+}
+void BinarySTree::editNode(string word,TreeNode* root){
+
+}
+
+//take a sorted vector and convert it to a binary search tree
+void BinarySTree::fill_tree(vector<string,int> words){
+
+}
+
+// this function return a vector of all words to display it 
+vector<pair<string,int>> BinarySTree:: display_tree(){
+    return display_tree(root);
+}
+vector<pair<string,int>> BinarySTree::display_tree(TreeNode*root){
+    if(!root)
+    return;
+    vector<pair<string,int>> words;
+    words=display_tree(root->right);
+    words.push_back({root->word,root->counter});
+
+    // how to concatenate two vectors?
+}
+
+
+// Destructor
---
+++ src/BST/binary_search_tree.hpp
@@ -0,0 +1,46 @@
+#ifndef BINARY_SEARCH_TREE_HPP
+#define BINARY_SEARCH_TREE_HPP
+#include <iostream>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::pair;
+
+struct TreeNode
+{
+    int counter{};
+    string word{""};
+    TreeNode*right{};
+    TreeNode*left{};
+
+    TreeNode(int counter,string word):counter(counter),word(word){}
+
+};
+
+class BinarySTree
+{
+private:
+    TreeNode *root{};
+public:
+    BinarySTree()=default;
+
+    void insert(string word,int counter);
+    void insert(string word,int counter,TreeNode* root);
+
+    void deleteNode(string word);
+    void deleteNode(string word,TreeNode* root);
+
+    void editNode(string word);
+    void editNode(string word,TreeNode* root);
+    
+    void fill_tree(vector<string,int> words);
+
+    vector<pair<string,int>> display_tree();
+    vector<pair<string,int>> display_tree(TreeNode*root);
+
+    ~BinarySTree();
+};
+
+
+#endif
